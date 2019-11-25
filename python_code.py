e73cab2177b5f0d7aded608899200f44ee8d66b8
nili1975 <58164427+nili1975@users.noreply.github.com>
2019-11-25 17:27:53 +0200

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,3 +1,6 @@
-iiiiiprint ("hello")
-print('hello')
 
+print('hello')
+## this notebook will be mainly used for the capstone project.
+##import pandas as pd
+##Import Numpy  as np
+Print ('Hello Capstone Project Course!')
